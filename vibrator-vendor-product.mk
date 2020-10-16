f7ce19e022bfc88f6e84004aeb6d03af2aec18ec
Alexander Koskovich <zvnexus@outlook.com>
2020-10-16 07:15:53 -0700

    vibrator: Fix board check.
     * is-board-platform-in-list is broken.
    
    Change-Id: I537d00b14e8b849de8040c32e1aaa4acf29fa686

--- vibrator-vendor-product.mk
+++ vibrator-vendor-product.mk
@@ -1,4 +1,4 @@
-ifeq ($(call is-board-platform-in-list, msmnile kona lahaina shima lito atoll sm6150 msm8953 msm8937 trinket bengal bengal_32 bengal_32go holi sdm660), true)
+ifneq ($(filter msmnile kona lahaina shima lito atoll sm6150 msm8953 msm8937 trinket bengal bengal_32 bengal_32go holi sdm660, $(TARGET_BOARD_PLATFORM)),)
 
 QTI_VIBRATOR_HAL_SERVICE := \
       vendor.qti.hardware.vibrator.service
