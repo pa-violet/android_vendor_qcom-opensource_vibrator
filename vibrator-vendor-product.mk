88e88b2e6f739b9476528f2340e888f13947e058
Alexander Koskovich <zvnexus@outlook.com>
2020-12-18 10:15:30 -0700

    Revert "vibrator: Fix board check."
    This reverts commit f7ce19e022bfc88f6e84004aeb6d03af2aec18ec.
    
    Change-Id: I7634b2e5a82883437591e431fac66125adf430e0

--- vibrator-vendor-product.mk
+++ vibrator-vendor-product.mk
@@ -1,4 +1,4 @@
-ifneq ($(filter msmnile kona lahaina shima lito atoll sm6150 msm8953 msm8937 trinket bengal bengal_32 bengal_32go holi sdm660, $(TARGET_BOARD_PLATFORM)),)
+ifeq ($(call is-board-platform-in-list, msmnile kona lahaina shima lito atoll sm6150 msm8953 msm8937 trinket bengal bengal_32 bengal_32go holi sdm660), true)
 
 QTI_VIBRATOR_HAL_SERVICE := \
       vendor.qti.hardware.vibrator.service
